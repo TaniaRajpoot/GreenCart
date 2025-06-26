f8bbbaa08612bf3d634bbc3bc3e9dae3be6a8e66
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-06-27 04:00:29 +0500

    fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
-import NewsLetter from '../components/newsLetter'
+import NewsLetter from '../components/NewsLetter'
 import Footer from '../components/Footer'
 
 const Home = () => {
