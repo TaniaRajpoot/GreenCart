96f3f01ae279eec2b18abff7c01aaced1acbba4d
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-06-27 04:06:30 +0500

    fix

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ await connectedDB()
 await connectCloudinary()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173','https://green-cart-bap1.vercel.app']
 
 app.post('/stripe', express.raw({type:'application/json'}) ,stripeWebHooks)
 
