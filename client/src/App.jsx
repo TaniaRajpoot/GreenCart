f783b4178dbe9904a808b65412183e5273169731
Tania Ashraf <151459794+TaniaRajpoot@users.noreply.github.com>
2025-06-27 12:26:35 +0500

    ,

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,6 +22,7 @@ const App = () => {
 
   const isSellerPath = useLocation().pathname.includes('seller');
   const {showUserLogin,isSeller} = useAppContext()
+console.log(import.meta.env.VITE_BACKEND_URL);
 
   return (
     <div className='text-default min-h-screen text-gray-700 bg-white'>
